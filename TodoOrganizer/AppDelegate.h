7fe4228931995be39a5fb8a85f36625ab20d3016
Apple <Apple@mahmood1s-mac-2.local>
2013-11-03 14:29:15 +0200

    Initial Commit

---
+++ TodoOrganizer/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  TodoOrganizer
+//
+//  Created by Apple on 11/3/13.
+//  Copyright (c) 2013 Some. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ TodoOrganizer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TodoOrganizer
+//
+//  Created by Apple on 11/3/13.
+//  Copyright (c) 2013 Some. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
